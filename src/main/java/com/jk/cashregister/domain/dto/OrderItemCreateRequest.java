4ad82716bf15101a46e4c4b91a2d90660e30f3d8
Jakub Koczaja <kuba.koczaja@gmail.com>
2022-07-27 22:58:43 +0200

    clean up, bean validation added

--- src/main/java/com/jk/cashregister/domain/dto/OrderItemCreateRequest.java
+++ src/main/java/com/jk/cashregister/domain/dto/OrderItemCreateRequest.java
@@ -3,15 +3,21 @@ package com.jk.cashregister.domain.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 
-@Component
+import javax.validation.constraints.Positive;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderItemCreateRequest {
+
+		@Positive(message = "Stock's id must be positive number")
 		private Long stockId;
+
+		@Positive(message = "Order's id must be positive number")
 		private Long orderId;
+
+		@Positive(message = "Quantity ordered must be positive number")
 		private int quantityOrdered;
 
 }
--- src/main/java/com/jk/cashregister/domain/dto/OrderOpenRequest.java
+++ src/main/java/com/jk/cashregister/domain/dto/OrderOpenRequest.java
@@ -3,16 +3,17 @@ package com.jk.cashregister.domain.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 
+import javax.validation.constraints.Positive;
 import java.time.LocalDateTime;
 
-@Component
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderOpenRequest {
 		private LocalDateTime openDate;
+
+		@Positive(message = "User's id must be positive number")
 		private Long userId;
 
 }
